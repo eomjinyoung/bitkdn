2880067cbd7013d3ea83b8e933b179a1f2a8ae3f
jinyoung <jinyoung.eom@gmail.com>
2015-02-04 13:54:44 +0900

    .

--- exam01t/src/com/kdn/bitedu/test/test08/Calculator3.java
+++ exam01t/src/com/kdn/bitedu/test/test08/Calculator3.java
@@ -4,6 +4,9 @@ import com.kdn.bitedu.test.test07.Calculator2;
 
 /* 수퍼 클래스의 메서드를 서브 클래스의 목적에 맞추어 재정의하기
  * - Overriding
+ * 
+ * 애노테이션?
+ * - 컴파일러나 JVM에게 전달하는 아주 특별한 주석
  */
 
 public class Calculator3 extends Calculator2 {
@@ -11,6 +14,7 @@ public class Calculator3 extends Calculator2 {
     result %= a;
   }
   
+  @Override
   public void divide(int a) {
     if (a == 0) {
       System.err.println("0으로 나눌 수 없습니다.");
