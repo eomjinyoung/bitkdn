6ea1ad4e5c1df9270773b12d48cc3d27f2e360e6
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-02-11 14:58:15 +0900

    .

--- web01/src/com/kdn/web/servlet/BoardInsert.java
+++ web01/src/com/kdn/web/servlet/BoardInsert.java
@@ -14,6 +14,9 @@ public class BoardInsert extends GenericServlet {
   @Override
   public void service(ServletRequest request, ServletResponse response)
       throws ServletException, IOException {
+    //한글이 깨지지 않도록 하려면, getParameter()를 최초로 호출하기 전에 
+    //입력 데이터의 인코딩 형식을 설정해야 한다.
+    request.setCharacterEncoding("UTF-8");
     String title = request.getParameter("title");
     String content = request.getParameter("content");
     
