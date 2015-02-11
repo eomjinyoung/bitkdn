e109d056b027981bea0a39353cdcfcacfe93bede
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-02-11 13:35:03 +0900

    .

---
+++ web01/src/com/kdn/web/servlet/BoardInsert.java
@@ -0,0 +1,29 @@
+package com.kdn.web.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class BoardInsert extends GenericServlet {
+
+  @Override
+  public void service(ServletRequest arg0, ServletResponse arg1)
+      throws ServletException, IOException {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
