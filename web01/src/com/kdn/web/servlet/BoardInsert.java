304da234eab6808c91bcd14cc45ceeb0b0d37f3d
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-02-11 14:36:01 +0900

    .

--- web01/src/com/kdn/web/servlet/BoardInsert.java
+++ web01/src/com/kdn/web/servlet/BoardInsert.java
@@ -6,13 +6,15 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet(urlPatterns="/board/insert")
 public class BoardInsert extends GenericServlet {
 
   @Override
   public void service(ServletRequest arg0, ServletResponse arg1)
       throws ServletException, IOException {
-    // TODO Auto-generated method stub
+    System.out.println("okok");
     
   }
 
