35a76a0aafb22b0bea5cd671ad1013ca7becca5a
Jinyoung Eom <jinyoung.eom@gmail.com>
2015-02-25 13:48:27 +0900

    .

--- web01/src/com/kdn/web/servlet/BoardDetail.java
+++ web01/src/com/kdn/web/servlet/BoardDetail.java
@@ -1,8 +1,8 @@
 package com.kdn.web.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,65 +20,17 @@ public class BoardDetail extends HttpServlet {
     int no = Integer.parseInt(request.getParameter("no"));
     
     response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-    out.println("<html><head><title>게시물 상세보기</title></head><body>");
-    out.println("<h1>게시물 상세정보</h1>");
-    
     try {
       BoardDao boardDao = new BoardDao();
       Board board = boardDao.detail(no);
       
-      if(board != null) { 
-        out.println("<form action='update' method='post'>");
-        out.println("<table border='1'>");
-        
-        out.println("<tr>");
-        out.println("<th>번호</th>");
-        out.println("<td><input type='text' name='no' readonly value='" 
-            + board.getNo() + "'></td>");
-        out.println("</tr>");
-        
-        out.println("<tr>");
-        out.println("<th>제목</th>");
-        out.println("<td><input type='text' name='title' size='50' value='" 
-            + board.getTitle() + "'></td>");
-        out.println("</tr>");
-        
-        out.println("<tr>");
-        out.println("<th>내용</th>");
-        out.println("<td><textarea name='content' rows='10' cols='60'>" 
-            + board.getContent() + "</textarea></td>");
-        out.println("</tr>");
-        
-        out.println("<tr>");
-        out.println("<th>등록일</th>");
-        out.println("<td>" + board.getCreateDate() + "</td>");
-        out.println("</tr>");
-        
-        out.println("<tr>");
-        out.println("<th>조회수</th>");
-        out.println("<td>" + board.getViewCount() + "</td>");
-        out.println("</tr>");
-        out.println("</table>");
-        out.println("<button>변경</button>");
-        out.println("<button type='button' onclick='deleteBoard();'>삭제</button>");
-        out.println("</form>");
-        
-        out.println("<script>");
-        out.println("  function deleteBoard() {");
-        out.println("    location.href='delete?no=" + board.getNo() + "';");
-        out.println("  }");
-        out.println("</script>");
-        
-      } else {
-        out.println("<p>지정된 번호의 게시물이 없습니다!</p>");
-      }
-      out.println("</body></html>");
+      request.setAttribute("board", board);
+      
+      RequestDispatcher rd = request.getRequestDispatcher("/board/Detail.jsp");
+      rd.include(request, response);
       
     } catch (Exception e) {
       e.printStackTrace();
-      
     } 
-
   }
 }
--- web01/src/com/kdn/web/servlet/BoardInsert.java
+++ web01/src/com/kdn/web/servlet/BoardInsert.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletResponse;
 
 import com.kdn.web.dao.BoardDao;
 import com.kdn.web.vo.Board;
@@ -30,21 +31,7 @@ public class BoardInsert extends GenericServlet {
       e.printStackTrace();
     }
     
-    //한글을 출력하기
-    //- 출력 스트림을 얻기 전에, 전송할 데이터의 형식과 문자집합을 설정한다.
-    response.setContentType("text/html;charset=UTF-8");
-    
-    // 웹 브라우저로 출력하기
-    PrintWriter out = response.getWriter();
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<meta http-equiv='Refresh' content='1;url=list'>");
-    out.println("<title>등록결과</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>등록 성공입니다!</h1>");
-    out.println("</body>");
-    out.println("</html>");
+    ((HttpServletResponse)response).sendRedirect("list");
   }
 
 }
